54fe57296c89325018c1daf9efcedb1d0f396375
Justiceiro1 <gd.vogel1512@gmail.com>
2021-03-17 16:48:20 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,273 @@
+const help = (prefix) => {
+	return `
+<══════════════════════>
+      *꧁✞︎🅝🅢➎-🅡🅞🅑🅞🅣✞︎꧂*
+ <══════════════════════>
+
+ ╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱ ▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰ ╯
+               蒙Bᴏᴛ蒙Mᴇɴᴜ蒙
+           Dᴏɴᴏ:✞︎🅝🅢➎-🅡🅞🅑🅞🅣✞︎
+       Cʀɪᴀᴅᴏʀ:𝓐𝓷𝓳𝓸
+┌─────────❶.❷────────
+║〘 INFORMAÇÕES 〙
+║
+╠✰ *🅝🅢➎-🅡🅞🅑🅞🅣* 
+╠✰ 𝐃𝐎𝐍𝐎: ꪖ​ꪀ​𝓳​ꪮ​
+╠✰ *wa.me/+5527996160640*
+╠✰ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+║
+╠══༒︎〘 ιитєℓιgєи¢ια 〙༒︎══
+║
+║☯︎ ➪ *${prefix}sticker*
+║☯︎ ➪ *${prefix}toimg*
+║☯︎ ➪ *${prefix}botjokes (memes aleatórios)*
+║☯︎ ➪ *${prefix}memeindo*
+║☯︎ ➪ *${prefix}tts*
+║☯︎ ➪ *${prefix}lolih [on]*
+║☯︎ ➪ *${prefix}nsfwloli [off]*
+║☯︎ ➪ *${prefix}url2img*
+║☯︎ ➪ *${prefix}leens [na legenda]*
+║☯︎ ➪ *${prefix}wait [na legenda]*
+║☯︎ ➪ *${prefix}setprefix*
+║☯︎ ➪ *${prefix}burst* [da um grau no audio]
+║☯︎ ➪ *${prefix}esquilo* [voz de esquilo]
+║
+╠══༒︎〘 συтяσѕ 〙༒︎══
+║
+║☯︎ ➪ *${prefix}linkgp*
+║☯︎ ➪ *${prefix}simih [1/0]*
+║☯︎ ➪ *${prefix}marcar*
+║☯︎ ➪ *${prefix}add [@]*
+║☯︎ ➪ *${prefix}banir [@]*
+║☯︎ ➪ *${prefix}promover [@]*
+║☯︎ ➪ *${prefix}rebaixar*
+║☯︎ ➪ *${prefix}admins*
+║☯︎ ➪ *${prefix}marcar2*
+║☯︎ ➪ *${prefix}bc [texto]* (ele faz uma ™)
+║☯︎ ➪ *${prefix}marcar3*
+║☯︎ ➪ *${prefix}bloqueados*
+║☯︎ ➪ *${prefix}bloquear [@]*
+║☯︎ ➪ *${prefix}desbloquear [@]*
+║☯︎ ➪ *${prefix}limpar*
+║☯︎ ➪ *${prefix}bc [ *texto* ]*
+║☯︎ ➪ *${prefix}bemvindo [1/0]*
+║☯︎ ➪ *${prefix}clonar [@]*
+║☯︎ ➪ *${prefix}help1*
+║☯︎ ➪ *${prefix}dono*
+║☯︎ ➪ *${prefix}owner*
+║☯︎ ➪ *${prefix}tts [texto]*
+║☯︎ ➪ *${prefix}setnome*
+║☯︎ ➪ *${prefix}termux*
+║☯︎ ➪ *${prefix}setfoto*
+║☯︎ ➪ *${prefix}grupoinfo*
+║☯︎ ➪ *${prefix}ytmp4*
+║☯︎ ➪ *${prefix}bomdia*
+║☯︎ ➪ *${prefix}boanoite*
+║☯︎ ➪ *${prefix}marcar*
+║☯︎ ➪ *${prefix}marcar2*
+║☯︎ ➪ *${prefix}marcar3*
+║
+╠══༒︎〘 ιитєℓιgєи¢ια 〙༒︎══
+║
+║☯︎ ➪ *${prefix}loli* [off]
+║☯︎ ➪ *${prefix}loli1*
+║☯︎ ➪ *${prefix}hentai*
+║☯︎ ➪ *${prefix}dono*
+║☯︎ ➪ *${prefix}porno*
+║☯︎ ➪ *${prefix}boanoite*
+║☯︎ ➪ *${prefix}bomdia*
+║☯︎ ➪ *${prefix}boatarde*
+║☯︎ ➪ *${prefix}mia [aleatórias]*
+║☯︎ ➪ *${prefix}rize [aleatórias]*
+║☯︎ ➪ *${prefix}minato [aleatórias]*
+║☯︎ ➪ *${prefix}boruto [aleatórias]*
+║☯︎ ➪ *${prefix}hinata [aleatórias]*
+║☯︎ ➪ *${prefix}sasuke [aleatórias]*
+║☯︎ ➪ *${prefix}sakura [aleatórias]*
+║☯︎ ➪ *${prefix}naruto [aleatórias]*
+║☯︎ ➪ *${prefix}meme*   
+║☯︎ ➪ *${prefix}lofi*
+║☯︎ ➪ *${prefix}malkova*
+║☯︎ ➪ *${prefix}canal*
+║☯︎ ➪ *${prefix}nsfwloli1*
+║☯︎ ➪ *${prefix}reislin*
+║
+╠══༒︎〘 ιитєℓιgєи¢ια 〙༒︎══
+║
+║☯︎ ➪ *${prefix}simih 1 (para ativar)*
+║☯︎ ➪ *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+╠══༒︎〘 єм ρяσ∂υ¢̧α̃σ 〙༒︎══
+║
+║☯︎ ➪ *${prefix}gado*
+║☯︎ ➪ *${prefix}dbz*
+║☯︎ ➪ *${prefix}gtts loli*
+║☯︎ ➪ *${prefix}hidegp*
+║
+╠══༒︎〘 ѕσ ρяємιυм 〙༒︎══
+║
+║☯︎ ➪ *${prefix}dado*
+║☯︎ ➪ *${prefix}cekvip*
+║☯︎ ➪ *${prefix}premiumlist*
+║☯︎ ➪ *${prefix}delete*
+║☯︎ ➪ *${prefix}modapk*
+║☯︎ ➪ *${prefix}indo10*
+║☯︎ ➪ *${prefix}daftarvip [para virar Premium]*
+║☯︎ ➪ *${prefix}qrcode*
+║☯︎ ➪ *${prefix}chentai*
+║☯︎ ➪ *${prefix}gcpf*
+║☯︎ ➪ *${prefix}gbin*
+║☯︎ ➪ *${prefix}pack*
+║☯︎ ➪ *${prefix}destrava*
+║☯︎ ➪ *${prefix}gpessoa*
+║
+╠══༒︎〘 gяυρσ 〙༒︎══
+║
+║☯︎ ➪ *${prefix}banir*
+║☯︎ ➪ *${prefix}leveling [on/off]*
+║☯︎ ➪ *${prefix}level*
+║☯︎ ➪ *${prefix}add*
+║☯︎ ➪ *${prefix}promover*
+║☯︎ ➪ *${prefix}setfoto [na legenda]*
+║☯︎ ➪ *${prefix}setname [texto]*
+║☯︎ ➪ *${prefix}rebaixar*
+║☯︎ ➪ *${prefix}admins*
+║☯︎ ➪ *${prefix}marcar*
+║☯︎ ➪ *${prefix}marcar2*
+║☯︎ ➪ *${prefix}marcar3*
+║☯︎ ➪ *${prefix}bemvindo [1/0]*
+║☯︎ ➪ *${prefix}grupoinfo*
+║☯︎ ➪ *${prefix}bomdia*
+║☯︎ ➪ *${prefix}boatarde*
+║☯︎ ➪ *${prefix}boanoite*
+║☯︎ ➪ *${prefix}setdesc*
+║☯︎ ➪ *${prefix}bug [sua mensagem]*
+║
+╠══༒︎〘 єѕρє¢ιfι¢σ ∂σ вσт 〙༒︎══
+║
+║☯︎ ➪ *${prefix}bug [sua mensagem]*
+║☯︎ ➪ *${prefix}clonar [@]*
+║☯︎ ➪ *${prefix}dono*
+║☯︎ ➪ *${prefix}ping [ver velocidade do bot]*
+║☯︎ ➪ *${prefix}termux*
+║☯︎ ➪ *${prefix}gay [@]*
+║☯︎ ➪ *${prefix}wame*
+║☯︎ ➪ *${prefix}map (nome)*
+║☯︎ ➪ *${prefix}setppbot (marque uma img)*
+║☯︎ ➪ *${prefix}pinterest (nome)*
+║☯︎ ➪ *${prefix}desligar (so para o dono)*
+║☯︎ ➪ *${prefix}timer*
+║
+╠══༒︎〘 мαιѕ αℓgυиѕ 〙༒︎══
+║
+║☯︎ ➪ *${prefix}neko*
+║☯︎ ➪ *${prefix}ttp [texto]*
+║☯︎ ➪ *${prefix}testime*
+║☯︎ ➪ *${prefix}tomp3*
+║☯︎ ➪ *${prefix}modoanime [on/off]*
+║☯︎ ➪ *${prefix}modonsfw [on/off]*
+║☯︎ ➪ *${prefix}happymod [jogo/app]*
+║☯︎ ➪ *${prefix}rize*
+║☯︎ ➪ *${prefix}ytsearch*
+║☯︎ ➪ *${prefix}moddroid [jogo/app]*
+║☯︎ ➪ *${prefix}xvideos [titulo]**
+║☯︎ ➪ *${prefix}nomegp*
+║☯︎ ➪ *${prefix}darkjokes (memes aleatórios)*
+║☯︎ ➪ *${prefix}animecry*
+║☯︎ ➪ *${prefix}gay1*
+║☯︎ ➪ *${prefix}next*
+║☯︎ ➪ *${prefix}alerta*
+║☯︎ ➪ *${prefix}belle [img aleatórias]*
+║☯︎ ➪ *${prefix}pronomeneu [texto]*
+║☯︎ ➪ *${prefix}hobby*
+║
+╠══༒︎〘 ¢σмαи∂σѕ ∂є νσz 〙༒︎══
+║
+║☯︎ ➪ *${prefix}ola*
+║☯︎ ➪ *${prefix}bv*
+║☯︎ ➪ *${prefix}tchau*
+║☯︎ ➪ *${prefix}bem*
+║☯︎ ➪ *${prefix}a*
+║☯︎ ➪ *${prefix}fdp*
+║☯︎ ➪ *${prefix}onich*
+║☯︎ ➪ *${prefix}beat1*
+║☯︎ ➪ *${prefix}glub*
+║
+╠══༒︎〘 συтяσѕ /2 〙༒︎══
+║
+║☯︎ ➪ *${prefix}antilink [1/0]*
+║☯︎ ➪ *${prefix}brainly [pergunta]*
+║☯︎ ➪ *${prefix}antiracismo [on/off]*
+║☯︎ ➪ *${prefix}setnomebot*
+║☯︎ ➪ *${prefix}meme*
+║
+╠══༒︎〘 ιитєяαтινσѕ 〙༒︎══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║☯︎ ➪ *bahc*
+║☯︎ ➪ *oii*
+║☯︎ ➪ *bv*
+║☯︎ ➪ *canta ai bot*
+║☯︎ ➪ *grita*
+║☯︎ ➪ *vingadorarindo*
+║☯︎ ➪ *peita*
+║☯︎ ➪ *nada*
+║☯︎ ➪ *burro*
+║☯︎ ➪ *kkkk*
+║☯︎ ➪ *nao*
+║☯︎ ➪ *cagao*
+║☯︎ ➪ *pau*
+║☯︎ ➪ *mesmo*
+║☯︎ ➪ *peida*
+║☯︎ ➪ *adoro*
+║☯︎ ➪ *ain*
+║☯︎ ➪ *bicha*
+║☯︎ ➪ *boiola*
+║☯︎ ➪ *bosta*
+║☯︎ ➪ *fogo*
+║☯︎ ➪ *maconha*
+║☯︎ ➪ *mlk*
+║☯︎ ➪ *maldade*
+║☯︎ ➪ *piranha*
+║☯︎ ➪ *ex*
+║☯︎ ➪ *galinha*
+║☯︎ ➪ *travazap*
+║☯︎ ➪ *rindoo*
+║☯︎ ➪ *sera*
+║☯︎ ➪ *morreu*
+║☯︎ ➪ *puta*
+║☯︎ ➪ *raspadinha*
+║☯︎ ➪ *sefudeu*
+║☯︎ ➪ *some*
+║☯︎ ➪ *suamae*
+║☯︎ ➪ *viado*
+║☯︎ ➪ *vsf*
+║☯︎ ➪ *wtf*
+║☯︎ ➪ *gemido*
+║☯︎ ➪ *geme*
+║☯︎ ➪ *bemm*
+║☯︎ ➪ *respeita*
+║☯︎ ➪ *chuvendo*
+║☯︎ ➪ *kkkk*
+║☯︎ ➪ *satanas*
+║
+╠══༒︎〘 єм тєѕтє 〙༒︎══
+║
+║  
+║  
+║
+╚═〘 🅝🅢➎-🅡🅞🅑🅞🅣 〙`
+}
+
+exports.help = help
+
